fc4100dadf43362da131200b3c6729226ad7f207
Your Name <you@example.com>
2023-04-20 21:26:28 +0700

    first commit

---
+++ cicilan.php
@@ -0,0 +1,151 @@
+<?php
+extract($_POST);
+error_reporting(0);
+
+$result = null;
+$terhutang= null;
+$bunga= null;
+$cicilan=null;
+
+
+
+
+// true && true = true
+// false && true = false
+// false && false = false
+
+// true || true = true
+// true || false = true
+// false || false = false
+// if($first == $second && $second == $third && $first == $third){
+//     $result = 'Segitiga sama sisi';
+// } else if($first == $second || $second == $third || $first == $third){
+//     $result = 'Segitiga sama kaki';
+// }else if(pow($first,2)==pow($second,2)+pow($third,2)||pow($second,2)==pow($first,2)+pow($third,2)||pow($third,2)==pow($second,2)+pow($first,2)){
+//     $result = 'Segitiga siku2';
+// } else{
+//     $result = 'Segitiga sembarang';
+// }
+    
+if($dp < $harga/5 || $harga<=$dp ){
+    $bulan=0;       $result='Anda tidak bisa Kredit !!! (DP Kurang)';
+}else{$result=''; $terhutang=$harga-$dp; $bunga=3/100*$terhutang; if($bulan>0){$cicilan=($terhutang)/($bulan)+($bunga);}}
+
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Cicilan</title>
+</head>
+
+<body>
+    <div class="container">
+        <form action="cicilan.php" method="POST" enctype="multipart/form-data" class="mt-2">
+            <div class="form-group">
+                <label for="" >Harga Motor</label>
+                <input type="number" class="form-control"  name="harga"  >
+            </div>
+            <div class="form-group">
+                <label for="">Bayar Pertama</label>
+                <input type="number" class="form-control"  name="dp">
+                
+            </div>
+            <div class="form-group">
+                <label for="">Bulan</label>
+                <br>
+                <select id="pilihan" name="bulan">
+                    <option value=12>12</option>
+                    <option value=24>24</option>
+                    <option value=36>36</option>
+                </select>
+                <input type="hidden" name="bunga">
+
+            </div>
+            <button type="submit" class="btn btn-info mt-3">Submit Form</button>
+        </form>
+        <!-- <div class="form-group">
+            <label for="fullname">Result</label>
+            <input type="text" class="form-control" value="<?php echo $result ?>">
+        </div> -->
+        <h4 class="text-danger"><?php echo $result ?></h4>
+        <h4> Harga=Rp. <?php echo number_format($harga, 0, ',', '.')?></h4>
+        <h4> Bayar Pertama=Rp. <?php echo number_format($dp, 0, ',', '.')?></h4>
+        <h4> Terhutang=Rp. <?php echo number_format($terhutang, 0, ',', '.')?></h4>
+        <h4> Bunga=Rp. <?php echo number_format($bunga, 0, ',', '.')?></h4>
+        <h4> Cicilan=Rp. <?php echo number_format($cicilan, 0, ',', '.') ?></h4>
+
+    </div>
+    <!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Tabel Cicilan</title>
+</head>
+<style>
+    
+    th, td {
+        border: 1px solid black;
+    }
+    table {
+        margin-left: auto;
+        margin-right: auto;
+        width: 80%;
+        border-collapse: collapse;
+}
+</style>
+
+  <table border="2" class="mb-5">
+    <thead>
+      <tr >
+        <th>Bulan</th>
+        <th>Cicilan</th>
+        <th>Terbayar</th>
+        <th>Kurang</th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php for ($i = 1; $i <= $bulan; $i++) { ?>
+      <tr>
+        <td><?php echo $i; ?></td>
+        <td>Rp. <?php echo number_format($cicilan, 0, ',', '.');?></td>
+        <td>Rp. <?php echo number_format(($cicilan*$i), 0, ',', '.');?></td>
+        <td>Rp. <?php echo number_format(($cicilan*$bulan)-($cicilan*$i), 0, ',', '.');?></td>
+      </tr>
+    <?php } ?>
+    </tbody>
+  </table>
+<body>
+
+</body>
+</html>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
